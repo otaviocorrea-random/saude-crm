c8b0f460157a9ed3f0c12aee68f7a4bbaf4bde49
Otavio Corrêa P. Silva <otaviocorrea@gmail.com>
2022-12-03 01:08:10 -0300

    adjust migraton name

--- db/migrate/20221202235541_create_estados.rb
+++ db/migrate/20221202235541_create_estados.rb
@@ -1,4 +1,4 @@
-class CreateUfs < ActiveRecord::Migration[7.0]
+class CreateEstados < ActiveRecord::Migration[7.0]
   def change
     create_table :estados do |t|
       t.string :sigla, null: false, limit: 2, index: { unique: true }
